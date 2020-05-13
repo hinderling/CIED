4f75ab1e7b732d21d5c85535334d509c59b2f309
timogi <ogi.tim@bluewin.ch>
2020-05-13 16:37:21 +0200

    function.py and tim_run.py added
    function.py now contains open()

--- functions.py
+++ functions.py
@@ -9,14 +9,11 @@ def open(path):
     Will return two lists of images as np arrays
     path is usually "DATA"
     """
-
     folders = glob(f"{path}/*")
-
     allpost = []
     allpre = []
     for folder in folders:
         allpost.append(np.asarray(Image.open(f"{folder}/{basename(folder)}post.png")))
         allpre.append(np.asarray(Image.open(f"{folder}/{basename(folder)}post.png")))
 
-
-    return (allpost, allpre)
+    return allpost, allpre
